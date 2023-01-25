610edb6ee522f3c7600d5abf51e77679d99e9ff8
johanlai <johanlai@hotmail.co.uk>
2023-01-25 14:19:09 +0000

    create multiple downloads and cleaned up

--- data_collection.py
+++ data_collection.py
@@ -10,9 +10,25 @@ import pandas as pd
 
 path = r"C:\Users\44756\Documents\ds_salary_project\chromedriver"
 slp_time = 2
-num_jobs = 1500
-keyword = 'Data analyst'
-
-df = gs.get_jobs(keyword, num_jobs, path, slp_time, False)
-
-df.to_csv('glassdoor_jobs.csv', index = False)
+save = 
+# Data analyst
+df = gs.get_jobs('Data analyst', 1, path, slp_time, False)
+df.to_csv('data/glassdoor_jobs_data_analyst.csv', index = False)
+# Quant developer
+df = get_jobs('Quantitative developer', 500, path, slp_time, False)
+df.to_csv('data/glassdoor_jobs_quant_developer.csv', index = False)
+# Python
+df = get_jobs('Python', 500, path, slp_time, False)
+df.to_csv('data/glassdoor_jobs_Python.csv', index = False)
+# Market risk
+df = get_jobs('market risk', 500, path, slp_time, False)
+df.to_csv('data/glassdoor_jobs_market_risk.csv', index = False)
+# Finance developer
+df = get_jobs('Finance developer', 500, path, slp_time, False)
+df.to_csv('data/glassdoor_jobs_findeveloper.csv', index = False)
+# Regression
+df = get_jobs('regression', 500, path, slp_time, False)
+df.to_csv('data/glassdoor_jobs_regression.csv', index = False)
+# Econometrics
+df = get_jobs('econometrics', 500, path, slp_time, False)
+df.to_csv('data/glassdoor_jobs_econometrics.csv', index = False)
