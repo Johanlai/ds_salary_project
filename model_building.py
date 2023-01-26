c5e677a07df9850819563136e334c15e7e0824c2
johanlai <johanlai@hotmail.co.uk>
2023-01-26 23:32:56 +0000

    Copied KJs model

--- model_building.py
+++ model_building.py
@@ -9,4 +9,101 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-df = pd.read_csv('cleaned_data.csv')
+df = pd.read_csv('cleaned_data.csv').dropna()
+
+df.columns
+
+df_model = df[['salary_thousands','Employees', 'revenue','comp_age','Company type','Company industry','Company sector', 
+               'simplified_jobs', 'seniority_jobs','em_provided_est', 'description_length',
+               'Career opportunities', 'Comp & Benefits', 'Culture & Values',
+               'Senior Management', 'Work/Life Balance', 'comp_rate',
+               'python_yn', 'R_yn', 'STATA_yn', 'SPSS_yn', 'spark_yn', 
+               'sql_yn', 'excel_yn', 'aws_yn', 'jupyter_yn']]
+
+df_dum = pd.get_dummies(df_model)
+
+df_dum.columns
+
+# train test split 
+from sklearn.model_selection import train_test_split
+
+X = df_dum.drop('salary_thousands', axis =1)
+y = df_dum.salary_thousands.values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# multiple linear regression 
+import statsmodels.api as sm
+
+X_sm = X = sm.add_constant(X)
+model = sm.OLS(y,X_sm)
+model.fit().summary()
+
+from sklearn.linear_model import LinearRegression, Lasso
+from sklearn.model_selection import cross_val_score
+
+lm = LinearRegression()
+lm.fit(X_train, y_train)
+
+np.mean(cross_val_score(lm,X_train,y_train, scoring = 'neg_mean_absolute_error', cv= 3))
+
+# lasso regression 
+lm_l = Lasso(alpha=.13)
+lm_l.fit(X_train,y_train)
+np.mean(cross_val_score(lm_l,X_train,y_train, scoring = 'neg_mean_absolute_error', cv= 3))
+
+alpha = []
+error = []
+
+for i in range(1,100):
+    alpha.append(i/100)
+    lml = Lasso(alpha=(i/100))
+    error.append(np.mean(cross_val_score(lml,X_train,y_train, scoring = 'neg_mean_absolute_error', cv= 3)))
+    
+plt.plot(alpha,error)
+
+err = tuple(zip(alpha,error))
+df_err = pd.DataFrame(err, columns = ['alpha','error'])
+df_err[df_err.error == max(df_err.error)]
+
+# random forest 
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor()
+
+np.mean(cross_val_score(rf,X_train,y_train,scoring = 'neg_mean_absolute_error', cv= 3))
+
+# tune models GridsearchCV 
+from sklearn.model_selection import GridSearchCV
+parameters = {'n_estimators':range(10,300,10), 'criterion':('squared_error','absolute_error'), 'max_features':('auto','sqrt','log2')}
+
+gs = GridSearchCV(rf,parameters,scoring='neg_mean_absolute_error',cv=3)
+gs.fit(X_train,y_train)
+
+gs.best_score_
+gs.best_estimator_
+
+# test ensembles 
+tpred_lm = lm.predict(X_test)
+tpred_lml = lm_l.predict(X_test)
+tpred_rf = gs.best_estimator_.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error
+mean_absolute_error(y_test,tpred_lm)
+mean_absolute_error(y_test,tpred_lml)
+mean_absolute_error(y_test,tpred_rf)
+
+mean_absolute_error(y_test,(tpred_lm+tpred_rf)/2)
+
+import pickle
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
+
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+
+model.predict(np.array(list(X_test.iloc[1,:])).reshape(1,-1))[0]
+
+list(X_test.iloc[1,:])
+print('a')
