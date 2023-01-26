23a0965d029287523775e3b36cf08a8f5a77d72b
johanlai <johanlai@hotmail.co.uk>
2023-01-26 19:34:57 +0000

    create model file

---
+++ model_building.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 26 19:27:13 2023
+
+@author: JHL
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+df = pd.read_csv('cleaned_data.csv')
